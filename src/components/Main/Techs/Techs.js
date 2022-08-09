c799323298ba626772a88d242367b9f13f33a870
Daria Gritsaenko <d.gritsaenko@articul.ru>
2022-08-10 00:14:05 +0300

    Адаптив блока 7 технологий

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -2,12 +2,12 @@ import './Techs.css';
 
 function Techs({componentRef}) {
   return (
-      <div ref={componentRef} className="techs">
+      <section ref={componentRef} className="techs">
         <div className="techs__container container">
           <h2 className="techs__title">Технологии</h2>
           <h3 className="techs__subtitle">7 технологий</h3>
           <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-          <ul className="techs__name">
+          <ul className="techs__list">
             <li className="techs__item">HTML</li>
             <li className="techs__item">CSS</li>
             <li className="techs__item">JS</li>
@@ -17,7 +17,7 @@ function Techs({componentRef}) {
             <li className="techs__item">mongoDB</li>
           </ul>
         </div>
-      </div>
+      </section>
   )
 }
 
