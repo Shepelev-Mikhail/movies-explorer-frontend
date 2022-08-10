c2a2f3c0eac67e9051579f1f352f21d34a099c51
Daria Gritsaenko <d.gritsaenko@articul.ru>
2022-08-10 20:22:35 +0300

    Страница 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,10 +5,16 @@ function NotFound() {
   const history = useHistory();
 
   return (
-    <div className="container">
-      404 ошибка
-
-      <div onClick={history.goBack}>Назад</div>
+    <div className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <button
+        type="button"
+        className="not-found__back"
+        onClick={history.goBack}
+      >
+        Назад
+      </button>
     </div>
   )
 }
